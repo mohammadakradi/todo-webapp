d2790e0067631f9ec1a09535b2469a425cd3cc9b
mohammadakradi <mohammad.akradi96@gmail.com>
2024-07-12 16:24:46 +0330

    app controller design updated

--- src/app/shared/components/app-controller/app-controller.component.ts
+++ src/app/shared/components/app-controller/app-controller.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-controller',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './app-controller.component.html',
   styleUrl: './app-controller.component.scss'
 })
